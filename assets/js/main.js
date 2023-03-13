f83919ce3a2409c8a69e4581f540ceae0a51acfb
Egyedi Róbert <egyedirobika@gmail.com>
2023-03-13 19:18:23 +0100

    Balra lapozás működik, alert-el jelzi ha nem tud balra lapozni

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,12 +5,24 @@ function btn()
     document.getElementById("jobb_oldal").innerHTML = szoveg.substring(325,649) 
 }
 
+var p = 0
 function lapoz_ballra()
 {
-    
+    console.log(p);
+    if (p<=0) 
+    {
+        alert("Erre a borító van")
+    }
+    else
+    {
+        p-= 2
+        var szoveg = document.getElementById("textarea").value
+        document.getElementById("bal_oldal").innerHTML = ""
+        document.getElementById("jobb_oldal").innerHTML = ""
+        document.getElementById("bal_oldal").innerHTML = szoveg.substring(324*-p, 324*-p+324)
+        document.getElementById("jobb_oldal").innerHTML = szoveg.substring(324*-p+324+1,324*-p+324+324)
+    }
 }
-
-var p = 0
 function lapoz_jobbra()
 {
     p+= 2
