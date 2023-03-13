b1c4d2fbe4bf24e983a4d410a6b47485801c9c3f
Egyedi Róbert <egyedirobika@gmail.com>
2023-03-13 14:49:03 +0100

    Jobbra lapoz balra nem

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,13 +10,13 @@ function lapoz_ballra()
     
 }
 
-var p = 1
+var p = 0
 function lapoz_jobbra()
 {
-    p++
+    p+= 2
     var szoveg = document.getElementById("textarea").value
     document.getElementById("bal_oldal").innerHTML = ""
     document.getElementById("jobb_oldal").innerHTML = ""
     document.getElementById("bal_oldal").innerHTML = szoveg.substring(324*p, 324*p+324)
-    document.getElementById("jobb_oldal").innerHTML = szoveg.substring(324*p+324+1,324*p+324+1)
+    document.getElementById("jobb_oldal").innerHTML = szoveg.substring(324*p+324+1,324*p+324+324)
 }
